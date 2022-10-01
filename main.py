8a4c78edc18a09b638612b944dd922832d77d622
Erica-Marie Carangalan <e.mariecara8@gmail.com>
2022-10-01 16:38:21 -0700

    Modified main.py and saved stories

---
+++ main.py
@@ -0,0 +1,88 @@
+
+
+my_hero = {
+    "name" : 'Pogi',
+    "level" : 1,
+    "health" : 100,
+    "equipment" : {'Invisible infinity pouch', 'Collar of the Gods', "Protector's Orb",},
+    "attacks" : (("sonic bark", 55), ("death stare", 30), 
+                ("pogi push", 25) ("love scratch", 50), ("pogi charge", 45),
+                 ('defend', 10)),
+    "coins" : {
+        "copper" : 50,
+        "silver" : 0,
+        "gold" : 0,
+    }
+}
+
+enemy_one ={
+    "name" : 'Fevmes, God of Fate',
+    "level" : 5,
+    "health" : 100,
+    "equipment" : {'Sling', 'Warped Seal',},
+    "attacks" : (('forsee', 20), ('lash', 25), ('firesling', 30)),
+    "coins" : {
+        "copper" : 5,
+        "silver" : 15,
+        "gold" : 2
+    }
+}
+
+enemy_two = {
+    "name" : 'Inagen, God of Life & Death',
+    "level" : 5,
+    "health" : 100, 
+    "equipment" : { 'Talisman of the Daywalker', 'Lifebinder', 'Ashes',},
+    "attacks" : (('mirror', 25), ('soultap', 50), ('decay', 40)),
+    "coins" : {
+        "copper" : 5,
+        "silver" : 15,
+        "gold" : 2
+    }
+}
+
+enemy_three = {
+    "name" : 'Phaztin, God of The Afterlife',
+    "level" : 5,
+    "health" : 100, 
+    "equipment" : {'Lantern of Whispers', 'Soulkeeper', 
+                    "Instrument of Hell's Games", },
+    "attacks" : (('dreamkiss', 20), ('nightkiss', 50), ('overwhelm', 25)),
+    "coins" : {
+        "copper" : 5,
+        "silver" : 15,
+        "gold" : 2
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ user_stories.py
@@ -0,0 +1,93 @@
+
+"""
+
+DATA ANALYST -----
+
+(5 points): As a data analyst, I want to make at least five commits
+             on GitHub with descriptive messages.
+(2.5 points): As a user, I want an engaging story to be told using 
+              print() statements. 
+(2.5 points): As a data analyst, I want to create a hero variable 
+            and set it equal to a Dictionary with the following keys and 
+            values. NOTE: The structure below indicates the name of the key 
+            on the left side of the colon and the value on the right side of
+             the colon. 
+            hero: Dictionary 
+                name: String 
+                level: Integer 
+                health: Integer 
+                equipment: Set of Strings 
+                attacks: Tuple of Tuples (each Tuple will have (attack_name: String, attack_power: Integer) 
+            coins: Dictionary 
+                copper: Integer 
+                silver: Integer 
+                gold: Integer 
+(2.5 points): As a data analyst, I want to create THREE enemy variables and 
+            set each equal to a Dictionary with the following keys and values.
+             NOTE: The structure below indicates the name of the key on the 
+             left side of the colon and the value on the right side of the 
+             colon. 
+            enemy: Dictionary 
+                name: String 
+                level: Integer 
+                health: Integer 
+                equipment: Set of Strings 
+                attacks: Tuple of Tuples (each Tuple will have (attack_name: String, attack_power: Integer) 
+            coins: Dictionary 
+                copper: Integer 
+                silver: Integer 
+                gold: Integer 
+
+
+USER --------
+
+(5 points): As a user, I want my Hero and Enemys attack to be chosen at 
+            random. 
+(5 points): As a user, I want my Hero or Enemys health to decrease based 
+            on the power of the successful attack. 
+(2.5 points): As a user, I want the results of each attack to be printed 
+            to the terminal.
+(5 points): As a user, I want the Hero and Enemy to continue attacking 
+            each other until one's health reaches zero.
+(5 points): As a user, I want to be able to “loot” defeated enemies by 
+        adding the Enemys equipment set to the Heros equipment Set
+
+
+ DATA ANALYST ----       
+(5 points): As a data analyst, I want the game to conclude when the Heros 
+        health reaches 0, or when all three Enemies have been defeated.   
+(2.5 points): As a data analyst, I want to create a run_game() function, 
+        which calls my other functions in a logical order to determine the 
+        games flow. 
+(5 points): As a data analyst, I want all of my functions to have a 
+    Single Responsibility. Remember, each function should do just one thing! 
+
+Bonus Stories
+(5 points): As a user, I want to “loot” defeated enemies by adding the 
+        Enemys gold, silver, and copper coins to the appropriate value 
+        in the Heros coin Dictionary.
+(7.5 points): As a user, I want to increase my Heros level after defeating 
+        an Enemy, which will include: 
+            Incrementing my Heros level by 1 
+            Adding a new attack of my choosing to my Hero's Attack Tuple. 
+
+
+
+
+
+
+
+
+
+
+"""
+
+
+
+
+
+
+
+
+
+
